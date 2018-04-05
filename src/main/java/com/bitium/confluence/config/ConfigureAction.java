e6b9e61739a091cdedf6a978017678bc60e75c9c
Andrei Tcibin <andrei_tcibin@epam.com>
2018-04-05 16:28:34 +0300

    feat: Replace loginUrl, logoutUrl, entityId and certificate with single metadata file

--- src/main/java/com/bitium/confluence/config/ConfigureAction.java
+++ src/main/java/com/bitium/confluence/config/ConfigureAction.java
@@ -22,33 +22,26 @@
 
 package com.bitium.confluence.config;
 
-import java.net.MalformedURLException;
-import java.net.URL;
+import com.opensymphony.webwork.ServletActionContext;
+import com.opensymphony.webwork.dispatcher.multipart.MultiPartRequestWrapper;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
-
 import com.atlassian.confluence.user.UserAccessor;
 import com.atlassian.spring.container.ContainerManager;
 import com.atlassian.user.Group;
 import org.apache.commons.lang.StringUtils;
-
 import com.atlassian.confluence.core.ConfluenceActionSupport;
-import com.bitium.saml.X509Utils;
 
 public class ConfigureAction extends ConfluenceActionSupport {
 	private static final long serialVersionUID = 1L;
 
-	private String loginUrl;
-	private String logoutUrl;
-	private String entityId;
 	private String autoCreateUser;
 	private String defaultAutoCreateUserGroup;
-	private String x509Certificate;
 	private String idpRequired;
 	private String redirectUrl;
 	private String maxAuthenticationAge;
 	private ArrayList<String> existingGroups;
-
 
 	private SAMLConfluenceConfig saml2Config;
 
@@ -67,44 +60,12 @@ public class ConfigureAction extends ConfluenceActionSupport {
 		this.idpRequired = idpRequired;
 	}
 
-	public String getX509Certificate() {
-		return x509Certificate;
-	}
-
-	public void setX509Certificate(String x509Certificate) {
-		this.x509Certificate = x509Certificate;
-	}
-
-	public String getEntityId() {
-		return entityId;
-	}
-
-	public void setEntityId(String entityId) {
-		this.entityId = entityId;
-	}
-
 	public String getAutoCreateUser() {
 		return autoCreateUser;
 	}
 
 	public void setAutoCreateUser(String autoCreateUser) {
 		this.autoCreateUser = autoCreateUser;
-	}
-
-	public String getLogoutUrl() {
-		return logoutUrl;
-	}
-
-	public void setLogoutUrl(String logoutUrl) {
-		this.logoutUrl = logoutUrl;
-	}
-
-	public String getLoginUrl() {
-		return loginUrl;
-	}
-
-	public void setLoginUrl(String loginUrl) {
-		this.loginUrl = loginUrl;
 	}
 
 	public String getRedirectUrl() {
@@ -146,6 +107,17 @@ public class ConfigureAction extends ConfluenceActionSupport {
 		this.existingGroups = existingGroups;
 	}
 
+	/**
+	 * @return Metadata federation file or null if none was passed to a form.
+     */
+    public File getFederationMetadata() {
+        final MultiPartRequestWrapper wrapper = (MultiPartRequestWrapper) ServletActionContext.getRequest();
+		final File[] files = wrapper.getFiles("metadata");
+        return files.length > 0
+                ? files[0]
+                : null;
+    }
+
 	protected List getPermissionTypes() {
 		List requiredPermissions = super.getPermissionTypes();
 		requiredPermissions.add("ADMINISTRATECONFLUENCE");
@@ -154,35 +126,8 @@ public class ConfigureAction extends ConfluenceActionSupport {
 
 	@Override
 	public void validate() {
-		if (StringUtils.isBlank(getLoginUrl())) {
-			addActionError(getText("saml2Plugin.admin.loginUrlEmpty"));
-		} else {
-			try {
-				new URL(getLoginUrl());
-			} catch (MalformedURLException e) {
-				addActionError(getText("saml2Plugin.admin.loginUrlInvalid"));
-			}
-		}
-		if (StringUtils.isBlank(getLogoutUrl())) {
-			// addActionError(getText("saml2Plugin.admin.logoutUrlEmpty"));
-		} else {
-			try {
-				new URL(getLogoutUrl());
-			} catch (MalformedURLException e) {
-				addActionError(getText("saml2Plugin.admin.logoutUrlInvalid"));
-			}
-		}
-		if (StringUtils.isBlank(getEntityId())) {
-			addActionError(getText("saml2Plugin.admin.entityIdEmpty"));
-		}
-		if (StringUtils.isBlank(getX509Certificate())) {
-			addActionError(getText("saml2Plugin.admin.x509CertificateEmpty"));
-		} else {
-			try {
-				X509Utils.generateX509Certificate(getX509Certificate());
-			} catch (Exception e) {
-				addActionError(getText("saml2Plugin.admin.x509CertificateInvalid"));
-			}
+		if (getFederationMetadata() == null) {
+			addActionError(getText("saml2Plugin.admin.metadataFileIsMissing"));
 		}
 		if (StringUtils.isBlank(getIdpRequired())) {
 			setIdpRequired("false");
@@ -203,10 +148,6 @@ public class ConfigureAction extends ConfluenceActionSupport {
 	}
 
 	public String doDefault() throws Exception {
-		setLoginUrl(saml2Config.getLoginUrl());
-		setLogoutUrl(saml2Config.getLogoutUrl());
-		setEntityId(saml2Config.getIdpEntityId());
-		setX509Certificate(saml2Config.getX509Certificate());
 		setRedirectUrl(saml2Config.getRedirectUrl());
 		long maxAuthenticationAge = saml2Config.getMaxAuthenticationAge();
 		
@@ -245,10 +186,7 @@ public class ConfigureAction extends ConfluenceActionSupport {
 	}
 
 	public String execute() throws Exception {
-		saml2Config.setLoginUrl(getLoginUrl());
-		saml2Config.setLogoutUrl(getLogoutUrl());
-		saml2Config.setEntityId(getEntityId());
-		saml2Config.setX509Certificate(getX509Certificate());
+		saml2Config.setFederationMetadata(getFederationMetadata());
 		saml2Config.setIdpRequired(getIdpRequired());
 		saml2Config.setRedirectUrl(getRedirectUrl());
 		saml2Config.setAutoCreateUser(getAutoCreateUser());
